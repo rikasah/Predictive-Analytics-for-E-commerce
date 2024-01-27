0add4192d42ff6bf73daeec5b39208a8d837de65
Rika Sahriana <64936742+rikasah@users.noreply.github.com>
2024-01-28 02:50:22 +0800

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 # Specify the Streamlit version
 import streamlit as st
-st.set_page_config(version="0.90.0")
+st.set_page_config(version="1.30.0")
 
 import streamlit as st
 import pandas as pd
